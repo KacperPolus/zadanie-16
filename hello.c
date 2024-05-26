283e06f487599a008cd357825afc07efbfe88369
Kacper Polus <kpolus79@gmail.com>
2024-05-26 13:17:15 +0200

    dane drugiej osoby

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    printf("Amadeusz Makowski\n");
+    printf("Hello, World!\n") ;
+    printf("Amadeusz Makowski\n") ;
+    printf("Kacper Polus\n") ;
     return(0);
 }
